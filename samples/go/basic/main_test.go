e797d9befe881aee52098e4b1d91496269004b50
ktr0328 <zf7heatpa@gmail.com>
2025-11-27 18:01:32 +0900

    Fix Go sample test output capture

--- samples/go/basic/main_test.go
+++ samples/go/basic/main_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"io"
 	"os"
 	"strings"
@@ -15,19 +14,21 @@ func TestSampleMain(t *testing.T) {
 	defer server.Close()
 	os.Setenv("PIPELINE_ENGINE_ADDR", server.Server.URL)
 
-	var buf bytes.Buffer
+	r, w, _ := os.Pipe()
 	stdout := os.Stdout
 	stderr := os.Stderr
-	os.Stdout = &buf
-	os.Stderr = &buf
+	os.Stdout = w
+	os.Stderr = w
 	defer func() {
 		os.Stdout = stdout
 		os.Stderr = stderr
+		_ = w.Close()
 	}()
 
 	main()
 
-	output, _ := io.ReadAll(&buf)
+	_ = w.Close()
+	output, _ := io.ReadAll(r)
 	text := string(output)
 	if !strings.Contains(text, "Submitting job") {
 		t.Fatalf("sample output missing expected text: %s", text)
